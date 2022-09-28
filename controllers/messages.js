93bf377c676e93b8d3613b40a462fa59f085d884
Cesar Desgarennes sanchez <l18350302@tuxtepec.tecnm.mx>
2022-09-28 08:53:17 -0500

    errores

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,24 +1,24 @@
 const { request, response } = require("express")
 
 const rootMessage = (req = request, res = response) => {
-    res.json({msg:"Hola buen dia :D!"})}
+    res.status(423).json({msg:"Hola buen dia :D!"})}
     
     
 const hiMessage =  (req = request, res = response) => {
-    res.json({msg:"Hola mundo!"})}
+    res.status(404).json({msg:"Hola mundo!"})}
 
 
 const byeMessage = (req = request, res = response) => {
-        res.json({msg:"Adios Mundo!"})}
+        res.status(418).json({msg:"adios mundo cruel!"})}
 
 const postMessage = (req = request, res = response) => {
-            res.json({msg:"Mensaje POST!"})}
+        res.status(402).json({msg:"Mensaje POST!"})}
 
-            const putMessage = (req = request, res = response) => {
-                res.json({msg:"mensaje PUT!"})}
+const putMessage = (req = request, res = response) => {
+        res.status(403).json({msg:"mensaje PUT!"})}
 
-                const deleteMessage = (req = request, res = response) => {
-                    res.json({msg:"mensaje delete"})}                
+const deleteMessage = (req = request, res = response) => {
+        res.status(404).json({msg:"mensaje delete"})}                
 
 
     module.exports = {rootMessage , 
@@ -27,4 +27,4 @@ const postMessage = (req = request, res = response) => {
         postMessage, 
         putMessage, 
         deleteMessage
-    }      
+    } 
