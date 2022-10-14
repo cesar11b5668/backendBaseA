d3b6881b8eb696f2ceba0e02c38c44c7ac53b717
Cesar Desgarennes sanchez <l18350302@tuxtepec.tecnm.mx>
2022-10-14 11:07:03 -0500

    se agrego pool

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,28 @@
 const { request, response } = require("express");
+const pool = require("../db/connection");
 
-const getUsers = (req = request, res = response) => {
-    console.log("funcion getUser")
-    res.json({msg: "Funcion getUsers"})
+const getUsers = async (req = request, res = response) => {
+ let conn;
+
+ try {
+    conn = await pool.getConnection()
+
+    const users = await conn.query("SELECT * FROM Usuarios", (error) => {throw new Error(error)})
+
+    if (!users) {
+        res.status(404).json({msg: "no se encontraron registros"})
+        return
+    }
+    res.json({users})
+ } catch (error) {
+    console.log(error)
+    res.status(500).json({error})
+ } finally{
+    if (conn) {
+        conn.end()
+    }
+ }
 }
+
 
 module.exports = {getUsers}
